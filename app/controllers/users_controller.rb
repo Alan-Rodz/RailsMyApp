1513b29b0bf98725763216edcff31c856ca7c05c
Alan Rodz <s061199@hotmail.com>
2026-02-01 16:23:02 -0600

    feat: implement dashboard functionality with controller, view, and updated routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Account created successfully!"
+      redirect_to dashboard_path, notice: "Account created successfully!"
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def new
-    @user = User.new
   end
 
   private
