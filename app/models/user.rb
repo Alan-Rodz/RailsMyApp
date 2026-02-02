d1cffb539f7fe648c4b22bb41b1aba4213fb86b5
Alan Rodz <s061199@hotmail.com>
2026-02-02 14:24:23 -0600

    feat: update User model to integrate Devise's encrypted_password and reset password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_secure_password
-
-  validates :email, presence: true, uniqueness: true
 end
